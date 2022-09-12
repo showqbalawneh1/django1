2d3eb5fd42c2e7a0aab2ff19c75336c61fe2461a
showq <shoooq-00123@hotmail.com>
2022-09-12 20:29:29 +0300

    task3

--- main/admin.py
+++ main/admin.py
@@ -3,17 +3,24 @@ from .models import question, choice
 
 # Register your models here.
 
+class choiceInline (admin.TabularInline):
+    model = choice
+    max_num = 10
+    extra = 1
+
 
 class choiceAdmin(admin.ModelAdmin):
     fields=["chText",
             "votes",
             "question"
             ]
-    
 class questionAdmin(admin.ModelAdmin):
     fields=["qText",
             "pupDate",
             ]
+    inlines = [
+        choiceInline,
+    ]
     
     
     
--- sensor/models.py
+++ sensor/models.py
@@ -1,23 +1,33 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from datetime import datetime 
 import django_filters
-
+import uuid
 
 
 # Create your models here.
 
 class pressureSensor(models.Model):
-    labelSensor =models.CharField(max_length=200)
+    def validation(labelSensor):
+        str = [*labelSensor]
+        if("PSSR" not in labelSensor[0:4]):
+            raise ValidationError(('labelSensor not valid should has a PSSR prefix '))
+
+    labelSensor =models.CharField(max_length=200,validators=[validation])
     installationDate=models.DateTimeField(auto_now_add=True)
     longitude= models.DecimalField(max_digits=9,decimal_places=6)
     latitude=models.DecimalField(max_digits=9,decimal_places=6)
+    serial_number=models.CharField(max_length=200,default=uuid.uuid4,editable=False)
+    
+ 
+
     def __str__(self):
         return self.labelSensor
     
     
 class sensorReading (models.Model):
     sensor=models.ForeignKey(pressureSensor, on_delete=models.CASCADE,null=False)
-    readingDate=models.DateTimeField(auto_now_add=True)
+    readingDate=models.DateTimeField(auto_now_add=True,editable=False)
     value=models.DecimalField(max_digits=19,decimal_places=10)
  
  
